5e79245cfa577e06aab62ca172291a38d416b818
Wouter Beeftink <wouter.beeftink@gmail.com>
2018-11-30 09:18:02 +0100

    Add router state composition

--- src/products/store/reducers/index.ts
+++ src/products/store/reducers/index.ts
@@ -1,8 +1,4 @@
-import {
-  ActionReducerMap,
-  createFeatureSelector,
-  createSelector
-} from '@ngrx/store';
+import { ActionReducerMap, createFeatureSelector } from '@ngrx/store';
 
 import * as fromPizzas from './pizzas.reducer';
 
@@ -15,29 +11,3 @@ export const reducers: ActionReducerMap<ProductsState> = {
 };
 
 export const getProductsState = createFeatureSelector<ProductsState>('products');
-
-// Pizzas state
-export const getPizzaState = createSelector(
-  getProductsState,
-  (state: ProductsState) => state.pizzas
-);
-
-export const getPizzasEntities = createSelector(
-  getPizzaState,
-  fromPizzas.getPizzasEntities
-);
-
-export const getAllPizzas = createSelector(
-  getPizzasEntities,
-  entities => Object.keys(entities).map(id => entities[parseInt(id, 10)])
-);
-
-export const getPizzasLoaded = createSelector(
-  getPizzaState,
-  fromPizzas.getPizzasLoaded
-);
-
-export const getPizzasLoading = createSelector(
-  getPizzaState,
-  fromPizzas.getPizzasLoading
-);
