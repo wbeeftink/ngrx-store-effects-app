c4a54a1e990c8cf9cd18f3c77ec8afef44d36ec4
Wouter Beeftink <wouter.beeftink@gmail.com>
2018-12-02 11:24:42 +0100

    Add route guards for preloading

--- src/products/containers/products/products.component.ts
+++ src/products/containers/products/products.component.ts
@@ -37,7 +37,5 @@ export class ProductsComponent implements OnInit {
 
   ngOnInit() {
     this.pizzas$ = this.store.select(fromStore.getAllPizzas);
-    this.store.dispatch(new fromStore.LoadPizzas());
-    this.store.dispatch(new fromStore.LoadToppings());
   }
 }
